1be276a1bc719cf3f2f488ab0b5219d4efa8174e
Sam Sherwood <sam@heya.llc>
2020-03-27 14:17:04 -0400

    Contentful Image Check
    Attempting to get preview environment working alongside SVG graphic

--- src/hooks/useContentfulFixedImage.js
+++ src/hooks/useContentfulFixedImage.js
@@ -1,22 +1,23 @@
 // useContentfulFixedImage.js
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from 'gatsby'
 
 export default assetUrl => {
-  const { allContentfulAsset } = useStaticQuery(
-    graphql`
-      query CONTENTFUL_IMAGE_FIXED_QUERY {
-        allContentfulAsset(filter: {file: {url: {ne: ""}}}) {
-          nodes {
-            file {
-              url
+    const { allContentfulAsset } = useStaticQuery(
+        graphql`
+            query CONTENTFUL_IMAGE_FIXED_QUERY {
+                allContentfulAsset(filter: { file: { url: { ne: "" } } }) {
+                    nodes {
+                        file {
+                            url
+                        }
+                        fixed(width: 1200, height: 630, cropFocus: FACES) {
+                            ...GatsbyContentfulFixed
+                        }
+                    }
+                }
             }
-            fixed(width: 1200, height: 630, cropFocus: FACES) {
-              ...GatsbyContentfulFixed
-            }
-          }
-        }
-      }
-    `
-  );
-  return allContentfulAsset.nodes.find(n => n.file.url === assetUrl).fixed;
-};
+        `
+    )
+
+    return allContentfulAsset.nodes.find(n => n.file.url === assetUrl).fixed
+}
--- src/hooks/useContentfulImage.js
+++ src/hooks/useContentfulImage.js
@@ -1,23 +1,31 @@
 // useContentfulImage.js
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from 'gatsby'
 
 export default assetUrl => {
-  const { allContentfulAsset } = useStaticQuery(
-    graphql`
-      query CONTENTFUL_IMAGE_QUERY {
-        allContentfulAsset(filter: {file: {url: {ne: ""}}}) {
-          nodes {
-            file {
-              url
+    const { allContentfulAsset } = useStaticQuery(
+        graphql`
+            query CONTENTFUL_IMAGE_QUERY {
+                allContentfulAsset(filter: { file: { url: { ne: "" } } }) {
+                    nodes {
+                        file {
+                            url
+                        }
+                        fluid(maxWidth: 1050, quality: 85) {
+                            ...GatsbyContentfulFluid_withWebp
+                        }
+                    }
+                }
             }
-            fluid(maxWidth: 1050, quality: 85) {
-              ...GatsbyContentfulFluid_withWebp
-            }
-          }
-        }
-      }
-    `
-  );
+        `
+    )
 
-  return allContentfulAsset.nodes.find(n => n.file.url === assetUrl).fluid;
-};
+    function isMatchedUrl(element, index, array) {
+        if (typeof element.file !== 'undefined' && element.file !== null) {
+            return element.file.url === assetUrl
+        } else {
+            return false
+        }
+    }
+
+    return allContentfulAsset.nodes.find(isMatchedUrl).fluid
+}
