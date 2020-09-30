b2bed67000d096cf6c3ee4187bf13abe7c465eb3
Sam Sherwood <sam@heya.llc>
2020-09-30 11:27:02 -0400

    Academic Offering Listings
    Now sort by alphabetical order on Short Title and Title

--- src/components/blocks/block-academic-offering-listing.js
+++ src/components/blocks/block-academic-offering-listing.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from 'gatsby'
 import get from 'lodash/get'
+import sortBy from 'lodash/sortBy'
 import RelatedOfferings from '../related-offerings'
 
 const BlockAcademicOfferingListing = ({ category, offeringType }, ...rest) => {
     const data = useStaticQuery(graphql`
         {
-            allContentfulAcademicOffering(filter: {offeringType: {ne: null}, category: {title: {ne: null}}}) {
+            allContentfulAcademicOffering(
+                filter: {
+                    offeringType: { ne: null }
+                    category: { title: { ne: null } }
+                }
+            ) {
                 edges {
                     node {
                         id
@@ -37,11 +43,21 @@ const BlockAcademicOfferingListing = ({ category, offeringType }, ...rest) => {
         return edge.node
     })
 
-    const FilterOfferings = (offering) => {
-        return (offering.category.slug === category.slug && offering.offeringType === offeringType);
+    const FilterOfferings = offering => {
+        return (
+            offering.category.slug === category.slug &&
+            offering.offeringType === offeringType
+        )
     }
-    
-    const filteredOfferings = offerings.filter(FilterOfferings)
+
+    const SortByTitle = offering => {
+        return typeof offering.shortTitle !== 'undefined' &&
+            offering.shortTitle !== null
+            ? offering.shortTitle
+            : offering.title
+    }
+
+    const filteredOfferings = sortBy(offerings.filter(FilterOfferings), SortByTitle)
 
     if (filteredOfferings != null && filteredOfferings.length > 0) {
         return <RelatedOfferings offerings={filteredOfferings} />
