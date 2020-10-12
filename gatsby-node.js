982ad3601c950d9c17cfe2f0a5f7f80feb15e861
Sam Sherwood <sam@heya.llc>
2020-10-12 10:44:15 -0400

    Redirect Module
    Netlify and in-client redirects for legacy URLs managed in Contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,237 +3,280 @@ const path = require('path')
 const linkResolver = require('./src/utils').linkResolver
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+    const { createPage, createRedirect } = actions
 
-  return new Promise((resolve, reject) => {
-    const siteroot = path.resolve('./src/templates/site-root.js')
-    const department = path.resolve('./src/templates/department.js')
-    const standardpage = path.resolve('./src/templates/standard-page.js')
-    const academicOffering = path.resolve('./src/templates/academic-offering.js')
-    const event = path.resolve('./src/templates/event.js')
-    const location = path.resolve('./src/templates/location.js')
-    const person = path.resolve('./src/templates/person.js')
+    return new Promise((resolve, reject) => {
+        const siteroot = path.resolve('./src/templates/site-root.js')
+        const department = path.resolve('./src/templates/department.js')
+        const standardpage = path.resolve('./src/templates/standard-page.js')
+        const academicOffering = path.resolve(
+            './src/templates/academic-offering.js'
+        )
+        const event = path.resolve('./src/templates/event.js')
+        const location = path.resolve('./src/templates/location.js')
+        const person = path.resolve('./src/templates/person.js')
 
-    resolve(
-      graphql(`
-      {
-        allContentfulHomepage {
-          edges {
-            node {
-              slug
-              title
-            }
-          }
-        }
-        allContentfulDepartment {
-          edges {
-            node {
-              slug
-              title
-              hidden
-              category {
-                slug
-              }
-            }
-          }
-        }
-        allContentfulStandardPage {
-          edges {
-            node {
-              title
-              slug
-              hidden
-              category {
-                slug
-              }
-            }
-          }
-        }
-        allContentfulAcademicOffering {
-          edges {
-            node {
-              slug
-              title
-              hidden
-              category {
-                slug
-              }
-            }
-          }
-        }
-        allContentfulLocation {
-          edges {
-            node {
-              title
-              slug
-              hidden
-              category {
-                slug
-              }
-            }
-          }
-        }
-        allContentfulPerson {
-          edges {
-            node {
-              title
-              slug
-              hidden
-            }
-          }
-        }
-        allContentfulEvent {
-          edges {
-            node {
-              title
-              slug
-              hidden
-              category {
-                slug
-              }
-            }
-          }
-        }
-      }
-      `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+        resolve(
+            graphql(`
+                {
+                    allContentfulHomepage {
+                        edges {
+                            node {
+                                slug
+                                title
+                            }
+                        }
+                    }
+                    allContentfulDepartment {
+                        edges {
+                            node {
+                                slug
+                                title
+                                hidden
+                                category {
+                                    slug
+                                }
+                            }
+                        }
+                    }
+                    allContentfulStandardPage {
+                        edges {
+                            node {
+                                title
+                                slug
+                                hidden
+                                category {
+                                    slug
+                                }
+                            }
+                        }
+                    }
+                    allContentfulAcademicOffering {
+                        edges {
+                            node {
+                                slug
+                                title
+                                hidden
+                                category {
+                                    slug
+                                }
+                            }
+                        }
+                    }
+                    allContentfulLocation {
+                        edges {
+                            node {
+                                title
+                                slug
+                                hidden
+                                category {
+                                    slug
+                                }
+                            }
+                        }
+                    }
+                    allContentfulPerson {
+                        edges {
+                            node {
+                                title
+                                slug
+                                hidden
+                            }
+                        }
+                    }
+                    allContentfulEvent {
+                        edges {
+                            node {
+                                title
+                                slug
+                                hidden
+                                category {
+                                    slug
+                                }
+                            }
+                        }
+                    }
+                    allContentfulPageRedirect {
+                        edges {
+                            node {
+                                id
+                                fromPath
+                                isPermanent
+                                toPath {
+                                    id
+                                    category {
+                                        category {
+                                            slug
+                                        }
+                                        slug
+                                    }
+                                    slug
+                                }
+                                statusCode
+                            }
+                        }
+                    }
+                }
+            `).then(result => {
+                if (result.errors) {
+                    console.log(result.errors)
+                    reject(result.errors)
+                }
 
-        const roots = result.data.allContentfulHomepage.edges
-        const departments = result.data.allContentfulDepartment.edges
-        const pages = result.data.allContentfulStandardPage.edges
-        const offerings = result.data.allContentfulAcademicOffering.edges
-        const locations = result.data.allContentfulLocation.edges
-        const people = result.data.allContentfulPerson.edges
-        const events = result.data.allContentfulEvent.edges
+                const roots = result.data.allContentfulHomepage.edges
+                const departments = result.data.allContentfulDepartment.edges
+                const pages = result.data.allContentfulStandardPage.edges
+                const offerings =
+                    result.data.allContentfulAcademicOffering.edges
+                const locations = result.data.allContentfulLocation.edges
+                const people = result.data.allContentfulPerson.edges
+                const events = result.data.allContentfulEvent.edges
 
-        roots.forEach((root, index) => {
-          let path = linkResolver.path(root.node)
+                const redirects = result.data.allContentfulPageRedirect.edges
 
-          createPage({
-            path: path,
-            component: siteroot,
-            context: {
-              slug: root.node.slug
-            },
-          })
-        })
+                roots.forEach((root, index) => {
+                    let path = linkResolver.path(root.node)
 
-        departments.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: siteroot,
+                        context: {
+                            slug: root.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                departments.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: department,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-        pages.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: department,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                pages.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: standardpage,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-        offerings.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: standardpage,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                offerings.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: academicOffering,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-        locations.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: academicOffering,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                locations.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: location,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-        people.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: location,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                people.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: person,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-        events.forEach((page, index) => {
-          if (page.node.hidden) {
-            return
-          }
+                    createPage({
+                        path: path,
+                        component: person,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
 
-          let path = linkResolver.path(page.node)
+                events.forEach((page, index) => {
+                    if (page.node.hidden) {
+                        return
+                    }
 
-          createPage({
-            path: path,
-            component: event,
-            context: {
-              slug: page.node.slug
-            },
-          })
-        })
+                    let path = linkResolver.path(page.node)
 
-      })
-    )
-  })
+                    createPage({
+                        path: path,
+                        component: event,
+                        context: {
+                            slug: page.node.slug,
+                        },
+                    })
+                })
+
+                redirects.forEach((redirect, index) => {
+                    if (
+                        redirect.node.fromPath === null ||
+                        redirect.node.toPath === null
+                    ) {
+                        return
+                    }
+
+                    const path = linkResolver.path(redirect.node.toPath)
+                    const isPermanent = redirect.node.isPermanent !== null ? redirect.node.isPermanent : false
+
+                    createRedirect({
+                        fromPath: redirect.node.fromPath,
+                        toPath: path,
+                        isPermanent: isPermanent,
+                        statusCode: redirect.node.statusCode,
+                        redirectInBrowser: true
+                    })
+                })
+            })
+        )
+    })
 }
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
     if (stage === 'build-html') {
-        actions.setWebpackConfig({ 
+        actions.setWebpackConfig({
             module: {
                 rules: [
-                  {
-                    test: /@typeform/,
-                    loader: 'null-loader',
-                  },
+                    {
+                        test: /@typeform/,
+                        loader: 'null-loader',
+                    },
                 ],
-              }
+            },
         })
     }
 }
