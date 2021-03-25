a7ce376ec0f03d670502b1cfdc573e0c5f591538
Sam Sherwood <sam@heya.llc>
2021-03-24 21:03:18 -0400

    Return Security Headers to Default

--- gatsby-config.js
+++ gatsby-config.js
@@ -137,7 +137,7 @@ module.exports = {
             options: {
                 headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
                 allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-                mergeSecurityHeaders: true, // boolean to turn off the default security headers
+                mergeSecurityHeaders: false, // boolean to turn off the default security headers
                 mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
                 mergeCachingHeaders: true, // boolean to turn off the default caching headers
                 transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
