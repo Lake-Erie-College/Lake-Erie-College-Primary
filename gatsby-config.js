c5f17700fa6cb9e820c79067bcde84c01cb07048
Sam Sherwood <sam@heya.llc>
2022-03-24 22:08:15 -0400

    Remove Service Worker
    Offline service removed as interim step to turning off feature.

--- gatsby-config.js
+++ gatsby-config.js
@@ -157,7 +157,7 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-offline`,
+            resolve: `gatsby-plugin-remove-serviceworker`,
         },
         {
             resolve: 'gatsby-plugin-google-tagmanager',
